3d2082329fa636dcd62321ff31e33558609de526
Natalie Taktachev <ntaktach@gmail.com>
2019-07-10 17:59:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0
   new_numbers = []
   while counter < numbers.length do
-    numbers[counter] ** 2
+    new_numbers << numbers[counter] ** 2
     counter += 1
   end
 
