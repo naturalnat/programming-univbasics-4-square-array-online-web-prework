177c8194df862450fe105f06c86cac472d71cfdd
Natalie Taktachev <ntaktach@gmail.com>
2019-07-10 17:05:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0
+
+  while counter < array.length do
+    array[counter] ** 2
+    counter += 1
+  end
+
+  square_array = array[counter] ** 2
+
 end
